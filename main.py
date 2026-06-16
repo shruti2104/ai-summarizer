320c59b2631bd9bebf250b170200864a9f1eb869
Shruti Sharma <shruti21.sharma04@gmail.com>
2026-06-16 17:08:38 +0530

    Pdf extractor and summarizer code

--- main.py
+++ main.py
@@ -1,21 +1,93 @@
 from dotenv import load_dotenv
 from openai import OpenAI
+from pypdf import PdfReader
 import os
+import json
+
 
 load_dotenv()
 
-api_key = os.getenv("OPENAI_API_KEY")
-
-client = OpenAI(api_key=api_key)
-
-response = client.chat.completions.create(
-    model="gpt-4.1-mini",
-    messages=[
-        {
-            "role": "user",
-            "content": "Summarize this: Revenue increased by 20% and expenses increased by 5%."
-        }
-    ]
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY")
 )
 
-print(response)
+
+def extract_text_from_pdf(file_path):
+    reader = PdfReader(file_path)
+
+    content = ""
+
+    for page in reader.pages:
+        text = page.extract_text()
+
+        if text:
+            content += text + "\n"
+
+    return content
+
+
+def summarize_text(text):
+
+    messages = [
+        {
+            "role": "system",
+            "content": """
+            You are a document summarizer.
+
+            Return a json object only.
+
+            Format:
+
+            {
+              "summary": "",
+              "key_points": [],
+              "action_items": []
+            }
+
+            Rules:
+            - Only use information from the document
+            - Do not add information
+            - Do not give advice
+            - Do not infer action items
+            """
+        },
+        {
+            "role": "user",
+            "content": text
+        }
+    ]
+
+
+    response = client.chat.completions.create(
+        model="gpt-4.1-mini",
+        temperature=0,
+        response_format={
+            "type": "json_object"
+        },
+        messages=messages
+    )
+
+
+    return json.loads(
+        response.choices[0].message.content
+    )
+
+
+def summarize_pdf(file_path):
+
+    text = extract_text_from_pdf(file_path)
+
+    result = summarize_text(text)
+
+    return result
+
+
+
+# Program starts here
+
+result = summarize_pdf("Pre-SaleBash.pdf")
+
+print(result)
+# print(summary["summary"])
+# print(summary["key_points"])
+# print(summary["action_items"])
